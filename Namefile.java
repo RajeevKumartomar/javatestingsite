d02d3cdcfec2dced442f21ef8263efdad404cb29
RajeevKumartomar <85306727+RajeevKumartomar@users.noreply.github.com>
2022-02-28 22:59:58 +0530

    Update Namefile.java

--- Namefile.java
+++ Namefile.java
@@ -6,11 +6,11 @@ public static void main()
 {
 Scanner s=new Scanner(System.in);
 System.out.println("Enter your name :");
-String st=Rajeev
+String st=Rajeev;
 System.out.println("Enter your age :");
-int age=34
+int age=34;
 System.out.println("Enter your year of birth :");
-String yr=1987
+String yr=1987;
 System.out.println("Name :"+st);
 System.out.println("Age :"+age);
 System.out.println("Year of birth :"+yr);
