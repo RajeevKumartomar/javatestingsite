21345b38b4a8013cbd62d6e08c184696891327c4
RajeevKumartomar <85306727+RajeevKumartomar@users.noreply.github.com>
2022-02-28 22:56:38 +0530

    Create Namefile.java

---
+++ Namefile.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+class Namefile
+{
+public static void main()
+{
+Scanner s=new Scanner(System.in);
+System.out.println("Enter your name :");
+String st=Rajeev
+System.out.println("Enter your age :");
+int age=34
+System.out.println("Enter your year of birth :");
+String yr=1987
+System.out.println("Name :"+st);
+System.out.println("Age :"+age);
+System.out.println("Year of birth :"+yr);
+}
+}
